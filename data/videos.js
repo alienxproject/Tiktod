2b0aa5091334cc3f0eee1fcd711e311ef5333e95
alienxproject <142660399+alienxproject@users.noreply.github.com>
2026-04-17 17:11:37 +0700

    Create videos.js

---
+++ data/videos.js
@@ -0,0 +1,20 @@
+var videos = [
+  {
+    id: 1,
+    src: "cdn2.aceimg.com/28bc64a81.mp4",
+    thumb: "https://i.pinimg.com/236x/ef/92/50/ef9250156267cd0dc392448caee151c3.jpg",
+    title: "Jangan tonton ini sendirian...",
+    views: "12K views",
+    duration: "8:32",
+    cta: "https://id.wikipedia.org/wiki/Setan_dalam_Islam"
+  },
+  {
+    id: 2,
+    src: "cdn2.aceimg.com/5a2fcc785.mp4",
+    thumb: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ4MqwCuTUaLPwx2O-J0HTQEduzy-fnEKyyz2B2NRy-A&s=10",
+    title: "Ternyata ini yang disembunyikan...",
+    views: "9.4K views",
+    duration: "6:11",
+    cta: "https://majalahnabawi.com/setan-manusia-lebih-berbahaya/"
+  }
+];
