d3d14fe992f8c82b085199a55478610636ef601f
alienxproject <142660399+alienxproject@users.noreply.github.com>
2026-04-17 20:59:24 +0700

    Update videos.js

--- data/videos.js
+++ data/videos.js
@@ -1,20 +1,119 @@
 var videos = [
   {
     id: 1,
-    src: "https://cdn2.aceimg.com/28bc64a81.mp4",
-    thumb: "https://i.pinimg.com/236x/ef/92/50/ef9250156267cd0dc392448caee151c3.jpg",
-    title: "Jangan tonton ini sendirian...",
-    views: "12K views",
+    src: "https://cdn.videy.co/23UfokTP1.mp4",
+    thumb: "",
+    title: "dede suka main bantal ",
+    views: "69k views",
     duration: "8:32",
     cta: "https://id.wikipedia.org/wiki/Setan_dalam_Islam"
   },
-  {
+    {
     id: 2,
-    src: "https://cdn2.aceimg.com/5a2fcc785.mp4",
+    src: "https://cdn2.aceimg.com/28bc64a81.mp4",
     thumb: "",
-    title: "Ternyata ini yang disembunyikan...",
+    title: "kalo di atas suka perih",
+    views: "59K views",
+    duration: "8:32",
+    cta: ""
+  },
+    {
+    id: 3,
+    src: "https://cdn2.aceimg.com/5a1a7bfe2.mp4",
+    thumb: "",
+    title: "buruan sayang sini",
+    views: "65K views",
+    duration: "8:32",
+    cta: ""
+  },
+    {
+    id: 4,
+    src: "https://cdn.videy.co/lP0S0ywP1.mp4",
+    thumb: "",
+    title: "majn sama anjingku",
+    views: "78K views",
+    duration: "8:32",
+    cta: ""
+  },
+    {
+    id: 5,
+    src: "https://cdn.videy.co/pxowQLTq1.mp4",
+    thumb: "",
+    title: "tete ku bagus ga",
+    views: "90K views",
+    duration: "8:32",
+    cta: ""
+  },
+    {
+    id: 6,
+    src: "https://cdn.videy.co/dOydMjXm1.mp4",
+    thumb: "",
+    title: "tahan jangan sampe keluar",
+    views: "34K views",
+    duration: "8:32",
+    cta: ""
+  },
+    {
+    id: 7,
+    src: "https://cdn.videy.co/V8tqEsVX1.mp4",
+    thumb: "",
+    title: "tetangga kos jago goyang",
+    views: "11K views",
+    duration: "8:32",
+    cta: ""
+  },
+    {
+    id: 8,
+    src: "https://cdn.videy.co/qzKEYw201.mp4",
+    thumb: "",
+    title: "kakak suka ngajakin main pas rumah sepi",
+    views: "71K views",
+    duration: "8:32",
+    cta: ""
+  },
+    {
+    id: 9,
+    src: "https://cdn.videy.co/EPKk7JuT1.mp4",
+    thumb: "",
+    title: "belum pernah ngewe ",
+    views: "62K views",
+    duration: "8:32",
+    cta: ""
+  },
+    {
+    id: 10,
+    src: "https://cdn.videy.co/KhegSmFj1.mp4",
+    thumb: "",
+    title: "gede banget sayang ga muat",
+    views: "82K views",
+    duration: "8:32",
+    cta: ""
+  },
+    {
+    id: 11,
+    src: "https://cdn.videy.co/vz8AUgB51.mp4",
+    thumb: "",
+    title: "goyangan wot mu sangat gila",
+    views: "12K views",
+    duration: "5:32",
+    cta: ""
+  },
+    {
+    id: 12,
+    src: "https://cdn.videy.co/2wv1ekzl1.mp4",
+    thumb: "",
+    title: "main sama abang emang nyaman",
+    views: "35K views",
+    duration: "20:12",
+    cta: ""
+  },
+  {
+    id: 13,
+    src: "https://cdn.videy.co/wbFIgDD81.mp4",
+    thumb: "",
+    title: "pelan pelan aja dek",
     views: "9.4K views",
     duration: "6:11",
-    cta: "https://majalahnabawi.com/setan-manusia-lebih-berbahaya/"
+    cta: ""
   }
 ];
